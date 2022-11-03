967a68e8964c16514e5c5fb6d39c6a2c8b25207c
Hieu Do <xlu.untitled@gmail.com>
2022-11-03 15:44:56 +0700

    Add info panel texts

---
+++ src/stations/InfoPanel.tsx
@@ -0,0 +1,43 @@
+import { Text } from '@react-three/drei'
+import { MeshProps } from '@react-three/fiber'
+import { BufferGeometry } from 'three'
+import { IInfoData } from './InfoPanelsData'
+
+interface IInfoPanelProps extends MeshProps {
+  infoData: IInfoData
+}
+
+const getBoundBox = (geometry?: BufferGeometry) => {
+  let width = 0
+  let height = 0
+  geometry?.computeBoundingBox()
+  if (geometry?.boundingBox) {
+    width = geometry.boundingBox.max.x - geometry.boundingBox.min.x
+    height = geometry.boundingBox.max.y - geometry.boundingBox.min.y
+  }
+
+  return { width, height }
+}
+
+export const InfoPanel = ({ geometry, infoData: { title, description, offsety = 0 }, ...props }: IInfoPanelProps) => {
+  const { width, height } = getBoundBox(geometry)
+
+  return (
+    <mesh geometry={geometry} {...props}>
+      <meshBasicMaterial color={'#040404'} transparent />
+      <Text
+        color="#ffffff"
+        fontSize={0.05}
+        renderOrder={1}
+        textAlign="left"
+        anchorX="left"
+        anchorY="top"
+        maxWidth={0.8 * width}
+        lineHeight={1.3}
+        position={[(-width / 2) * 0.82, (height / 2) * 0.65 + offsety, 0.01]}
+      >
+        {title + '\n \n' + description}
+      </Text>
+    </mesh>
+  )
+}
