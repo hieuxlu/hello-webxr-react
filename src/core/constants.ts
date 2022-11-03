ef5e1f4e06e29b31805eb316f7b94abca1fc74bb
Hieu Do <xlu.untitled@gmail.com>
2022-11-03 20:47:38 +0700

    Handle PUBLIC_URL for Github Pages

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,2 +1,3 @@
-export const BASIS_LIB_PATH = '/vendor/'
-export const ASSETS_PATH = '/assets/'
+export const PUBLIC_URL = process.env.PUBLIC_URL || ''
+export const BASIS_LIB_PATH = PUBLIC_URL + '/vendor/'
+export const ASSETS_PATH = PUBLIC_URL + '/assets/'
